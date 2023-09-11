a3c5bfc58dc32b8bc9a6e4daab82474d605e96ae
Sérgio Fagundes <contato.sergiofagundes@gmail.com>
2023-09-11 02:12:53 -0300

    Colocando livros em 3d, e mudando o banner

--- src/Components/Livro.js
+++ src/Components/Livro.js
@@ -4,13 +4,25 @@ import './Livro.css';
 function Livro({ livro }) {
   return (
     <div className="livro">
-      <img src={livro.Capa} alt={livro.Nome}/>
+      <div class="book">
+      <div class="book-cover">
+        <img src={livro.Capa} />
+      </div>
+      <div class="book-spine">
+        <h1>BOOKSHARE</h1>
+      </div>
+    </div>
+      <br />
       <h3>{livro.Nome}</h3>
       <p>Autor: {livro.Autor}</p>
       <p>Gênero: {livro.Genero}</p>
       <p>Preço: {livro.Preco}</p>
       <p>Vendas: {livro.Vendas}</p>
     </div>
+
+    
+
+
   );
 }
 
--- src/Home/App.js
+++ src/Home/App.js
@@ -40,6 +40,9 @@ function App() {
                 <h1 className='title'>BookShare</h1>
                 <h4 className='slogan'>Here you will explore all the worlds of literature.</h4>
               </div>
+              <div className="image-box">
+                <img src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/School-Clipart/Paper_Plane_PNG_Clip_Art_Image.png?m=1469069701" alt="" />
+              </div>
             </div>
           </header>
       </div>
